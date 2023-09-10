3f282cf77978548bfdda5ad9b73715c06c2b14ce
seano <sean.mccully@voteshield.us>
2023-09-10 23:01:02 +0000

    more friendly error pages

---
+++ errorpages/error-417.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{516:function(e,n,o){"use strict";o.r(n);o(109);var s=o(50);Object(s.default)((()=>{const e=document.querySelector("img");e.addEventListener("mouseenter",(()=>{e.src="/oops.gif"})),e.addEventListener("mouseleave",(()=>{e.src="/oops.png"}))}))}},[[516,0]]]);
+//# sourceMappingURL=error-d8c6552c2591d9814e28.chunk.js.map
